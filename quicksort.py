e4272a1743c0bfa0eafff1394e8c3384513056b2
hasstariq <68686454+hasstariq@users.noreply.github.com>
2020-11-18 05:52:45 -0500

    Create quicksort.py

---
+++ quicksort.py
@@ -0,0 +1,13 @@
+def quickSort(arr, low, high):
+    if len(arr) == 1:
+        return arr
+    if low < high:
+ 
+        # pi is partitioning index, arr[p] is now
+        # at right place
+        pi = partition(arr, low, high)
+ 
+        # Separately sort elements before
+        # partition and after partition
+        quickSort(arr, low, pi-1)
+        quickSort(arr, pi+1, high)
